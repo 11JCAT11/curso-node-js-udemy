7327ed34db8ec9e860b8ff284262989cd77c5029
JuankDev <11jcat11@gmail.com>
2023-04-22 13:34:15 -0400

    Subiendo la segunda seccion del curso

---
+++ 02-fundamentos/const-var-let.js
@@ -0,0 +1,7 @@
+const nombre = 'Wolverine';
+if(true){
+    const nombre = 'magneto';
+    // console .log(nombre);
+}
+// var nombre;
+console.log(nombre);
---
+++ 02-fundamentos/desestructuracion.js
@@ -0,0 +1,25 @@
+const deadpool = {
+    nombre: 'Wade',
+    apellido: 'Windton',
+    poder: 'Regeneracion',
+    getName (){
+        return `${this.nombre} ${this.apellido} ${this.poder}`
+    }
+}
+// const nombre = deadpool.nombre;
+// const apellido = deadpool.apellido;
+// const poder = deadpool.poder;
+
+
+function imprimeHeroe ({ nombre, apellido, poder, edad = 0  }) {
+    // const { nombre, apellido, poder, edad = 0 } = heroe;
+    console.log(nombre, apellido, poder , edad);
+}
+// imprimeHeroe(deadpool);
+const heroes = ['Deadpool', 'Superman', 'Batman'];
+
+// const h1 = heroes[0];
+// const h2 = heroes[1];
+// const h3 = heroes[2];
+const [ , , h3] = heroes;
+console.log( h3 );
